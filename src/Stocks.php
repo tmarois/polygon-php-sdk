8df0ed5c5a4c621701232097daf4bdec67edba03
Timothy Marois <timothymarois@gmail.com>
2020-02-17 22:29:56 -0500

    Update to ticks v2

--- src/Stocks.php
+++ src/Stocks.php
@@ -129,7 +129,7 @@ class Stocks
         ],$this->timeout)->contents();
 
         // return (array) $contents['results'] ?? [];
-        return (array) $contents['ticks'] ?? [];
+        return (array) $contents['results'] ?? [];
     }
 
     /**
